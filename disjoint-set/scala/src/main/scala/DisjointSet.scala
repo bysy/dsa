dbb8bee34717410f9bf4b9fdae92bf1b051e104b
Benjamin <bysy@users.noreply.github.com>
2015-09-25 12:43:06 -0500

    feat: Keep tree more balanced

--- disjoint-set/scala/src/main/scala/DisjointSet.scala
+++ disjoint-set/scala/src/main/scala/DisjointSet.scala
@@ -8,11 +8,26 @@ class DisjointSet private (
     parents.zipWithIndex.count { case (p, i) => p==i }
   def isRoot(i: Int): Boolean = i==parents(i)
   def root(i: Int): Int = if (isRoot(i)) i else root(parents(i))
+  def rootAndDepth(i: Int): (Int,Int) = {
+    def run(j: Int, n: Int): (Int,Int) =
+      if (isRoot(j)) (j,n) else run(parents(j), n+1)
+    run(i, 0)
+  }
   def union(a: Int, b: Int): DisjointSet = {
+    val (rootA, depthA) = rootAndDepth(a)
+    val (rootB, depthB) = rootAndDepth(b)
+    if (depthA<depthB)
+      parents(rootA) = rootB
+    else
+      parents(rootB) = rootA
+    this
+  }
+  def unionSlow(a: Int, b: Int): DisjointSet = {
     parents(root(a)) = root(b)
     this
   }
   def areConnected(a: Int, b: Int): Boolean = root(a)==root(b)
+  def getParents = parents.clone()
 }
 
 object DisjointSet {
